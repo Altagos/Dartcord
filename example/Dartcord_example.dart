e62bdc2adf9a4fa630ce21e6b661d031362cd105
Altagos <altagos@posteo.de>
2020-06-15 17:14:05 +0200

    Project init
    stagehand init

---
+++ example/Dartcord_example.dart
@@ -0,0 +1,6 @@
+import 'package:Dartcord/Dartcord.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/Dartcord.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library Dartcord;
+
+export 'src/Dartcord_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
