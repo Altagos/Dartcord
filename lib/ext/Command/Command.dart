16ebd9aa55e87a254b26175653c74ba342e8531e
Altagos <altagos@posteo.de>
2020-06-26 22:05:10 +0200

    command #3

--- lib/ext/Command/Command.dart
+++ lib/ext/Command/Command.dart
@@ -1,14 +1,15 @@
 part of datcord_ext_command;
 
 class Command {
-   String _name;
+  String _name;
   Function _function;
   bool _disabled = false;
-   String _description;
-   List<String> _aliases;
-   bool _hidden;
+  String _description;
+  List<String> _aliases;
+  bool _hidden;
 
-  Command(String name, String description, List<String> aliases, bool hidden, {Function function}) {
+  Command(String name, String description, List<String> aliases, bool hidden,
+      {Function function}) {
     _name = name;
     _description = description;
     _aliases = aliases;
@@ -17,8 +18,14 @@ class Command {
   }
 
   String get name => _name;
+  String get description => _description;
+  List<String> get aliases => _aliases;
+  bool get hidden => _hidden;
+  bool get disabled => _disabled;
+  Function get function => _function;
 
+  set description(String description) => _description = description;
+  set aliases(List<String> aliases) => _aliases = aliases;
   set function(Function function) => _function = function;
-
   set disabled(bool disabled) => _disabled = disabled;
 }
