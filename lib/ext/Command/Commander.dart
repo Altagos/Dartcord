8cc00acb9db067e1dd0502635fc9797d9aeec898
Altagos <altagos@posteo.de>
2020-06-26 22:17:07 +0200

    command #5

--- lib/ext/Command/Commander.dart
+++ lib/ext/Command/Commander.dart
@@ -6,6 +6,15 @@ class Commander extends Client {
 
   Commander(String token, this._prefix) : super(token) {
     _commandLoader = CommandLoader(this);
+    on<MessageCreateEvent>().listen((MessageCreateEvent event) {
+      if (event.message.content.startsWith(_prefix) && event.message.author.id != user.id) {
+        var message = event.message.content.split(' ');
+        var command = message[0].substring(_prefix.length);
+        print(command);
+      } else {
+        return super.on<MessageCreateEvent>();
+      }
+    });
   }
 
   CommandLoader get commandLoader => _commandLoader;
