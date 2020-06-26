cfabc68ff7b78e6b459803944838c0dca10dec31
Altagos <altagos@posteo.de>
2020-06-26 22:13:41 +0200

    command #4

--- lib/ext/Command/Commander.dart
+++ lib/ext/Command/Commander.dart
@@ -2,10 +2,27 @@ part of datcord_ext_command;
 
 class Commander extends Client {
   CommandLoader _commandLoader;
+  final String _prefix;
 
-  Commander(String token) : super(token) {
+  Commander(String token, this._prefix) : super(token) {
     _commandLoader = CommandLoader(this);
   }
 
   CommandLoader get commandLoader => _commandLoader;
+
+  @override
+  Stream<T> on<T>() {
+    if (T is MessageCreateEvent) {
+      on<MessageCreateEvent>().listen((MessageCreateEvent event) {
+        if (event.message.content.startsWith(_prefix) && event.message.author.id != user.id) {
+          var message = event.message.content.split(' ');
+          var command = message[0].substring(_prefix.length);
+          print(command);
+        } else {
+          return super.on<T>();
+        }
+      });
+    }
+    return super.on<T>();
+  }
 }
