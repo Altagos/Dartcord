5585e3912eda5de2cc03c5f546a46465fc52826f
Altagos <altagos@posteo.de>
2020-06-26 18:59:32 +0200

    added hidden attribut to CommandClass

--- lib/ext/Command/CommandClass.dart
+++ lib/ext/Command/CommandClass.dart
@@ -4,11 +4,13 @@ abstract class CommandClass {
   String _name;
   String _description;
   List<String> _aliases;
+  bool _hidden;
 
-  CommandClass({String name, String description, List<String> aliases }) {
+  CommandClass(String name, { String description, List<String> aliases, bool hidden }) {
     _name = name;
     _description = description;
     _aliases = aliases;
+    _hidden = hidden;
   }
 
   void run() {}
